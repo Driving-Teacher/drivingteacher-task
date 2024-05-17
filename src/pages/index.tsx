75c9ca4618674700999ea46dbe62bf8f13b96747
Woojin-Choi <panda1996@snu.ac.kr>
2024-05-17 13:21:40 +0900

    Feat: fetch lesson data from API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+
 const SelectLesson = () => {
+  useEffect(() => {
+    // fetch lesson data from API
+    (async () => {
+      const Resp = await fetch("/api/get-lesson");
+      const fetchedData = await Resp.json();
+    })();
+  }, []);
+
   return <div>SelectLesson</div>;
 };
 
