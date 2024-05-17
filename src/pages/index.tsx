40f56fa19bfb2a5a65bd786b32938ffafc09f52c
Yoon Han <yoontopia94@gmail.com>
2024-05-17 19:06:50 +0900

    feat: server-side data fetching 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-const SelectLesson = () => {
-  return <div>SelectLesson</div>;
+import cn from 'classnames'
+import type { GetServerSideProps } from 'next'
+import type { Lesson } from './api/get-lesson'
+import { BASE_URL } from '@/constants/api'
+
+export interface SelectLessonPageProps {
+  data: Lesson[]
+}
+
+export const getServerSideProps: GetServerSideProps<SelectLessonPageProps> = (async () => {
+  const response = await fetch(`${BASE_URL}/api/get-lesson`)
+  const data = await response.json()
+  return {
+    props: { data }
+  }
+})
+
+const SelectLessonPage = ({ data }: SelectLessonPageProps) => {
+  return (
+    <div className={cn('w-full h-full', 'p-3')}>
+    </div>
+  );
 };
 
-export default SelectLesson;
+export default SelectLessonPage;
